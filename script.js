73537b92eb9a818440ccb6adfc08b2ed5c38f2f1
Péter Gyenge <peter.gyenge@boxygroup.hu>
2023-11-16 15:03:12 +0100

    add beautifire

--- script.js
+++ script.js
@@ -9,13 +9,29 @@ function isJSONValid(jsonText) {
 
 const jsonInput = document.getElementById("jsonInput");
 const validateButton = document.getElementById("validateButton");
+const beautifierButton = document.getElementById("beautifierButton");
 const result = document.getElementById("result");
 
+
+let jsonInputValue = ""
+
 validateButton.addEventListener("click", () => {
-  const inputText = jsonInput.value;
-  if (isJSONValid(inputText)) {
+  const jsonInputValue = jsonInput.value;
+  if (isJSONValid(jsonInputValue)) {
     result.textContent = "A JSON érvényes.";
   } else {
     result.textContent = "A JSON érvénytelen.";
+  }
+});
+
+
+beautifierButton.addEventListener("click", () => {
+  jsonInputValue = jsonInput.value;
+  try {
+    const data = JSON.parse(jsonInputValue);
+    const beautifiedJSON = JSON.stringify(data, null, 2);
+    jsonInput.value = beautifiedJSON;
+  } catch (error) {
+alert("valami nem jó")
   }
 });
